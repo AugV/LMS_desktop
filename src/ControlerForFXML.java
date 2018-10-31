25166ed85e7d657c830ad357a19cc9bf45e13c25
AUGUSTINAS VAINIUS <augustinas.vainius@gmail.com>
2018-10-31 20:47:39 +0200

    add window added, pops up, functions not implemented yet

--- src/ControlerForFXML.java
+++ src/ControlerForFXML.java
@@ -1,28 +1,32 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ControlerForFXML extends TabPane  {
-
     University university;
 
-    public void setUniversity(University university) {
-        System.out.println("ayeee");
-        this.university = university;
-    }
-
     @FXML
-    private Button button;
+    private Button btAddTeacher;
+    @FXML
+    private Button btSave;
 
     @FXML
     private ListView teacherListView;
@@ -44,6 +48,24 @@ public class ControlerForFXML extends TabPane  {
     @FXML
     private void initialize(){
         teacherListView.setItems(new Utility().objectListToObservableList(university.getTeacherList()));
+
+        btAddTeacher.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getClassLoader().getResource("TeacherAddWindow.fxml"));
+                    Stage stage = new Stage();
+                    stage.setTitle("My New Stage Title");
+                    stage.setScene(new Scene(root, 450, 450));
+                    stage.show();
+                    // Hide this current window (if this is what you want)
+                    ((Node)(event.getSource())).getScene().getWindow().hide();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
 
