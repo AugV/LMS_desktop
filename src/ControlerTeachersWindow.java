65612c51875ea7b577ad66ec6c8d3a4a13a8882d
AUGUSTINAS VAINIUS <augustinas.vainius@gmail.com>
2018-11-02 15:59:55 +0200

    nulls from listview eliminated

--- src/ControlerTeachersWindow.java
+++ src/ControlerTeachersWindow.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
@@ -26,39 +27,46 @@ public class ControlerTeachersWindow extends TabPane {
     }
 
 
-
     @FXML
     private void initialize() {
         teacherListView.getItems().addAll(university.getTeacherList());
 
-        teacherListView.setCellFactory(lv->{
+
+        teacherListView.setCellFactory(lv -> {
+
             ListCell<Teacher> cell = new ListCell<>();
             ContextMenu contextMenu = new ContextMenu();
-
             MenuItem deleteItem = new MenuItem();
-
             deleteItem.textProperty().bind(Bindings.format("Delete"));
             deleteItem.setOnAction(event -> teacherListView.getItems().remove(cell.getItem()));
             contextMenu.getItems().addAll(deleteItem);
+            Text text = new Text();
+            cell.setGraphic(text);
+            text.textProperty().bind(Bindings.createStringBinding(() -> {
+                if (cell.isEmpty()) {
+                    return null ;
+                } else {
+                    return cell.getItem().toString();
+                }
+            }, cell.emptyProperty(), cell.itemProperty()));
 
-            cell.textProperty().bind(cell.itemProperty().asString());
             cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
                 if (isNowEmpty) {
-                    cell.setContextMenu(null);
+                    cell.setContextMenu(contextMenu);
                 } else {
                     cell.setContextMenu(contextMenu);
                 }
             });
-            return cell ;
-
+            return cell;
         });
 
 
         btAddTeacher.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 createNewController();
-            }});
-        }
+            }
+        });
+    }
 
     private void setUpTheLoader() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ApplicationInterface.fxml"));
@@ -70,13 +78,14 @@ public class ControlerTeachersWindow extends TabPane {
             e.printStackTrace();
         }
     }
+
     private void createNewController() {
-        ControlerTeacherAdd addWindowController = new ControlerTeacherAdd(university,this);
+        ControlerTeacherAdd addWindowController = new ControlerTeacherAdd(university, this);
     }
 
-    public void updateListView(){
+    public void updateListView() {
         teacherListView.setItems(new Utility().objectListToObservableList(university.getTeacherList()));
         System.out.println("TeacherList updated");
     }
 
-    }
+}
