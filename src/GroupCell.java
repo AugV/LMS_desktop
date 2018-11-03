4aa1cabfe7a0adfb3b8ce789ea9d75de920366df
AUGUSTINAS VAINIUS <augustinas.vainius@gmail.com>
2018-11-03 22:13:10 +0200

    further

---
+++ src/GroupCell.java
@@ -0,0 +1,47 @@
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class GroupCell extends CustomCell {
+    public GroupCell(ListView listView, ParentController superControler, University university) {
+        super(listView, superControler, university);
+    }
+
+    public void createSelections() {
+        MenuItem deletionItem = new MenuItem();
+        deletionItem.textProperty().bind(Bindings.format("Delete"));
+        deletionItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                university.removeCourse(cell.getItem());
+                superControler.updateCourseListView();
+            }
+        });
+
+        contextMenu.getItems().addAll(deletionItem);
+    }
+
+    @Override
+    protected void cellUpdateMethodOverride() {
+        //super.cellUpdateMethodOverride();
+        cell = new ListCell<Group>() {
+            @Override
+            protected void updateItem(Group item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    text.setText(null);
+                } else {
+                    text.setText(item.getId() + ". ");
+                }
+            }
+        };
+    }
+}
