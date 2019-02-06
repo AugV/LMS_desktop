8f2c737f9045aba2f8741466702258cbdf902b8e
AUGUSTINAS VAINIUS <augustinas.vainius@gmail.com>
2019-02-06 16:48:06 +0200

    DataFiller Class added

--- src/main/java/main/ElDienynas.java
+++ src/main/java/main/ElDienynas.java
@@ -16,10 +16,6 @@ public class ElDienynas extends Application {
     static{entityManagerFactory = Persistence.createEntityManagerFactory("PersistenceUnitHibernateH2");}
 
     public void init() {
-//        setUniversity("VGTU");
-//        entityInstantiation();
-//        persistUniversityToDB();
-//        university= null;
         getUniversityFromDB();
 
     }
@@ -53,40 +49,9 @@ public class ElDienynas extends Application {
         entityManager.getTransaction().commit();
         entityManager.close();
     }
-//TODO reaorganize package structure
-    private void entityInstantiation() {
-        university.addTeacher("Tadas", "jablinksis");
-        university.addTeacher("Laimonas", "Stanislovsksi");
-
-        university.addGroup("GRUPE1");
-        university.addGroup("GRUPE2");
-
-
-        university.getStudentsGroupList().get(0).addGroupStudents(new entities.Student("Petras", "Studentauskas"));
-        university.getStudentsGroupList().get(1).addGroupStudents(new entities.Student("Studenis", "Studavicius"));
-
-        university.addCourse(new entities.Course(
-                "matematika", "Matematikos kursas", university.getTeacherList().get(0), university.getStudentsGroupList().get(0)));
-        university.addCourse(new entities.Course(
-                "darbaiXD", "Darbuko salalai", university.getTeacherList().get(1), university.getStudentsGroupList().get(1)));
-        university.addCourse(new entities.Course(
-                "darkoks velnias", "dar vienas niekam nereikalingas kursas", university.getTeacherList().get(1), university.getStudentsGroupList().get(1)));
-
-        entities.Task task1 = new entities.Task("primasTaskas", "ejozaselse per ezereli", "08-09", "5" );
-        university.getCourseList().get(0).addCourseTask(task1);
-
-        entities.Task task2 = new entities.Task("antrasTaskas", "ezerelis ejo per zaselse", "12-12", "1");
-        university.getCourseList().get(1).addCourseTask(task2);
-
-//        university.getCourseList().get(0).getCourseTasks().get(0).addTaskCompletedTask(new CompletedTask(
-//                 "teisingas atsakymas, kolegos"));
-//        university.getCourseList().get(1).getCourseTasks().get(0).addTaskCompletedTask(new CompletedTask(
-//                 "neteisingas atsakymas, biciuli"));
-    }
 
     public void stop() {
        mergeUniversityToDB();
-        //new SerializeDeserialize().serialize(university, "universityObject.ser");
     }
 
     private void mergeUniversityToDB() {
